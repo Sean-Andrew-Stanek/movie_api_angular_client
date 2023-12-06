ebc949384a2e1697de1f930495738c191305851a
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-12-06 22:03:51 +0900

    Changed to a module-based Angular design

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+//API source
+const apiURL = 'https://my-movie-db-1195f41cc20f.herokuapp.com'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserRegistrationService {
+
+    //Inject the HttpClient Moducle into the constructor params
+    //It will be accessable to the whole class
+    constructor(private http: HttpClient) {}
+
+    //Make the api call for the user registration endpoint
+    public userRegistration(userDetails: any): Observable<any> {
+        console.log(userDetails);
+        //TODO:  Either understand or fix this
+        //possibly 'users'=> '/users' or fix the apiURL to end in a '/'
+        return this.http.post(apiURL+'users',userDetails).pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(error: HttpErrorResponse): any {
+        if(error.error instanceof ErrorEvent) {
+            console.error('An error has occurred: ', error.error.message)
+        } else {
+            console.error(
+                `Error Status Code:\n    ${error.status},\n` + 
+                `Error body is: \n    ${error.error}`
+            )
+        }
+    }
+
+}
+
+
+
+export class FetchApiDataService {
+
+  constructor() { }
+}
