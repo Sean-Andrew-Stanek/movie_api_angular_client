a123f3eed6d6b38b94a5b5587590728346433bc8
Sean Andrew Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-12-18 17:58:19 +0900

    Similar Cards

--- src/app/director-card/director-card.component.ts
+++ src/app/director-card/director-card.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
-
+import { DataService } from '../data.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-director-card',
@@ -11,13 +13,19 @@ import { DatePipe } from '@angular/common';
 export class DirectorCardComponent {
     
     director: any;
+    similarMovies: any[];
 
     constructor(
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private datePipe: DatePipe
+        private datePipe: DatePipe,
+        private dataService: DataService,
+        private dialog: MatDialog,
+        private dialogRef: MatDialogRef<MovieCardComponent>,
         ) {
             this.director = this.data.director;
+            this.similarMovies = dataService.filteredMovies('director', this.director.name).slice(0,5);
         }
+        
 
     ngOnInit(): void {
         if(this.director.birth)
@@ -29,6 +37,15 @@ export class DirectorCardComponent {
         //Generic image if none is provided
         if(!this.director.image)
             this.director.image = 'assets/director.PNG';
+    }
+
+    openMovieCardDialog(movie: any): void {
+        this.dialog.open(MovieCardComponent, {
+            width: "80%",
+            height: "80%",
+            data: {movie}
+        });
+        this.dialogRef.close();
     }
 
 }
